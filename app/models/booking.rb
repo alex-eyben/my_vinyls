6c9f65760564b5187b75fb06cbffed9555386563
Alexandra Eyben <alexeyben@hotmail.com>
2020-05-18 17:54:51 +0200

    validations and associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :vinyl
   belongs_to :user
+  validates :date_begin, :date_end, presence: true
 end
--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,3 +1,5 @@
 class Vinyl < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :title, :artist, :release_date, :address, presence: true
 end
