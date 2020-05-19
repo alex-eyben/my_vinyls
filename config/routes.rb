f68d4ae71b36c1b900a52bf7f4f7c789c42d0cb8
Alexandra Eyben <alexeyben@hotmail.com>
2020-05-19 12:23:53 +0200

    bookings routes controller view and adding seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'bookings/new', to: 'bookings#new'
+  post 'bookings', to: 'bookings#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Vinyl.destroy_all
+
+puts 'Creating vinyls...'
+beatles = { title: "Abbey Road", artist: "The Beatles", release_date: 1969, address: "London", user_id: 1 }
+stones = { title: "Aftermath", artist: "The Rolling Stones", release_date: 1966, address: "Paris", user_id: 1 }
+
+[ beatles, stones ].each do |attributes|
+  vinyl = Vinyl.create!(attributes)
+  puts "Created #{vinyl.title}"
+end
+puts "Finished!"
+
