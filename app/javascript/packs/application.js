b416c4d4df2fd9b14682a77b53091a7344af13f6
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-05-21 20:48:45 +0200

    dashboard tabs only working on page load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,3 +38,25 @@ document.addEventListener('turbolinks:load', () => {
   initAutocomplete();
   initMapbox();
 });
+
+const tabs = document.querySelectorAll(".tabs-selector");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    event.preventDefault();
+    const currentActiveTabSelector = document.querySelector(".tabs-selector-active");
+    const currentInactiveTabSelector = document.querySelector(".tabs-selector-inactive");
+    const currentActiveTab = document.querySelector(".active-tab");
+    const currentInactiveTab = document.querySelector(".inactive-tab");
+    if (currentActiveTabSelector !== event.currentTarget) {
+      currentActiveTabSelector.classList.toggle("tabs-selector-active");
+      currentActiveTabSelector.classList.toggle("tabs-selector-inactive");
+      currentInactiveTabSelector.classList.toggle("tabs-selector-active");
+      currentInactiveTabSelector.classList.toggle("tabs-selector-inactive");
+      currentActiveTab.classList.toggle("active-tab");
+      currentActiveTab.classList.toggle("inactive-tab");
+      currentInactiveTab.classList.toggle("active-tab");
+      currentInactiveTab.classList.toggle("inactive-tab");
+    };
+  });
+});
