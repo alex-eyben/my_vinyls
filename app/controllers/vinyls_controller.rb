8644f4d8330d4b4319c452990f3d56c3b1d12b46
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-05-21 12:20:51 +0200

    vinyl routes complete, starting front in dashboard

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -7,4 +7,35 @@ class VinylsController < ApplicationController
     @vinyl = Vinyl.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @vinyl = Vinyl.new
+  end
+
+  def create
+    @vinyl = Vinyl.new(vinyl_params)
+    @vinyl.user = current_user
+    if @vinyl.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @vinyl = Vinyl.find(params[:id])
+  end
+  
+  def update
+    @vinyl = Vinyl.find(params[:id])
+    @vinyl.update(vinyl_params)
+    redirect_to dashboard_path
+  end
+
+  private
+  
+  def vinyl_params
+    params.require(:vinyl).permit(:title, :artist, :release_date, :address, :photo)
+  end
 end
